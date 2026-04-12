1cd1295d67691441c5caf52338ba95b9dcc79994
Michal Ozimek <s33994@pjwstk.edu.pl>
2026-04-12 17:33:46 +0200

    Fix: Decouple SubscriptionRenewalService from LegacyBillingGateway. Added Billing Gateway interface and LegacyBillingGatewayAdapter.

---
+++ LegacyRenewalApp/Interfaces/IBillingGateway.cs
@@ -0,0 +1,13 @@
+﻿namespace LegacyRenewalApp.Interfaces;
+
+public interface IBillingGateway
+{
+    void SaveInvoice(RenewalInvoice invoice);
+    void SendEmail(string email, string subject, string body);
+}
+
+public class LegacyBillingGatewayAdapter : IBillingGateway
+{
+    public void SaveInvoice(RenewalInvoice invoice) => LegacyBillingGateway.SaveInvoice(invoice);
+    public void SendEmail(string email, string subject, string body) => LegacyBillingGateway.SendEmail(email, subject, body);
+}
